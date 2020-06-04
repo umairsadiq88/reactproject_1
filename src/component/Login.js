1ae300993fb5a230c3f7403438bfdab245e2f101
umairsadiq88 <umairsadiq2010@gmail.com>
2020-06-04 12:55:27 +0500

    update login component

--- src/component/Login.js
+++ src/component/Login.js
@@ -3,12 +3,13 @@ import React from "react";
 function Login() {
   return (
     <div className="App">
+      <h1> Welcome! my Login Page</h1>
 
       <h1> Login Form</h1>
 
       <form>
-        <input type="text" placeholder="username"/> <br />
-        <input type="password" placeholder="password"/> <br />
+        <input type="text" placeholder="username" /> <br />
+        <input type="password" placeholder="password" /> <br />
         <input type="submit" /> <br />
       </form>
     </div>
